bae6c5ac051ee2c65451b6095761babe4dc68f8a
Nicolas Corredor <ncorredorc@hotmail.com>
2025-07-20 20:45:07 -0500

    functional to do list

---
+++ app.js
@@ -0,0 +1,61 @@
+const formulario = document.getElementById("formulario");
+const cards = document.getElementById("cards_content");
+
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //Creación de la estructura de mi tarjeta
+  const card = document.createElement("div");
+  card.className = "card";
+  const card_top = document.createElement("div");
+  card_top.className = "card_top";
+  const card_bot = document.createElement("div");
+  card_bot.className = "card_bot";
+  card.append(card_top, card_bot);
+  const card_title = document.createElement("h1");
+  card_title.className = "card_title";
+  const card_date = document.createElement("p");
+  card_date.className = "card_date";
+  const card_content = document.createElement("p");
+  card_content.className = "card_content";
+  card_top.append(card_title);
+  card_bot.append(card_content, card_date);
+
+  const { botonEditar, botonEliminar } = deleteandedit();
+  const card_top2 = document.createElement("div");
+  card_top2.id = "card_top2";
+  card_top2.append(botonEditar, botonEliminar);
+  console.log(card_top2);
+  card_top.append(card_top2);
+  //Toma de valores del formulario y asignación a sus etiquetas
+  const titulo = document.getElementById("form_title").value;
+  const info = document.getElementById("info").value;
+  const time = new Date().toLocaleString();
+
+  card_title.textContent = titulo;
+  card_content.textContent = info;
+  card_date.textContent = time;
+  console.log(card);
+  cards.append(card);
+});
+
+function deleteandedit() {
+  //creación del botón editar
+  const btn_editar = document.createElement("button");
+  btn_editar.className = "btn_editar";
+  btn_editar.id = "editar";
+  const icono1 = document.createElement("i");
+  icono1.className = "bi bi-pencil-square";
+  btn_editar.append(icono1);
+  //Creación del botón eliminar
+
+  const btn_eliminar = document.createElement("button");
+  btn_eliminar.className = "btn_eliminar";
+  btn_eliminar.id = "eliminar";
+  const icono2 = document.createElement("i");
+  icono2.className = "bi bi-trash3";
+  btn_eliminar.append(icono2);
+  return {
+    botonEditar: btn_editar,
+    botonEliminar: btn_eliminar,
+  };
+}
